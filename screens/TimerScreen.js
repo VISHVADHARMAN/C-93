a9614260078c270c2620174b3e8b8de5f0fcbaa7
VISHVADHARMAN <67373113+VISHVADHARMAN@users.noreply.github.com>
2021-02-09 19:47:48 +0530

    Add files via upload

---
+++ screens/TimerScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+export default class TimerScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Timer</Text>
+            </View>
+        );
+    };
+};
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+});
